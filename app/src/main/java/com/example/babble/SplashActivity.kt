41c0017c9dc9398d29f2b258d0247ba99bcead31
dylandasilva1999 <55394640+dylandasilva1999@users.noreply.github.com>
2021-05-05 21:56:31 +0200

    Implement Splash Screen
    -Added splash screen minimal animation.
    
    -Added fonts and custom colors
    
    -Added babble logo in as a vector asset

---
+++ app/src/main/java/com/example/babble/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.babble
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        logo_splash.alpha = 0f
+        logo_splash.animate().setDuration(1500).alpha(1f).withEndAction {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }
+
+    }
+}
