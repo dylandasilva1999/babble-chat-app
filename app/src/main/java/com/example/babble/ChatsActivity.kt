fa0525e12dcbf4646268effc2a318bb9066f3a48
dylandasilva1999 <55394640+dylandasilva1999@users.noreply.github.com>
2021-05-13 00:48:15 +0200

    Add Basic Drawer Menu

--- app/src/main/java/com/example/babble/ChatsActivity.kt
+++ app/src/main/java/com/example/babble/ChatsActivity.kt
@@ -2,18 +2,46 @@ package com.example.babble
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.babble.fragments.ChatsFragment
 import com.example.babble.fragments.SearchFragment
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_chats.*
+import kotlinx.android.synthetic.main.nav_header.*
 
 class ChatsActivity : AppCompatActivity() {
+
+    private lateinit var drawer: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chats)
 
         //Make the View FullScreen
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        //Get drawer by ID
+        drawer = findViewById(R.id.drawer_layout)
+        var navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        //Menu Icon
+        var menu_icon = findViewById<ImageView>(R.id.burger_menu)
+
+        //Toggle for navigation drawer open and close
+        var toggle: ActionBarDrawerToggle = ActionBarDrawerToggle(this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        //On Menu Icon Click Open Menu
+        menu_icon.setOnClickListener {
+            drawer.openDrawer(GravityCompat.START)
+        }
 
         //Reference the Chats and Search Fragments
         val chatsFragment = ChatsFragment()
@@ -45,6 +73,35 @@ class ChatsActivity : AppCompatActivity() {
                 commit()
             }
         }
+    }
 
+    val mOnNavigationItemSelectedListener = NavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_profile -> {
+                //Add Profile Intent Activity Here
+                drawer.closeDrawer(GravityCompat.START)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_chats-> {
+                //Add Chats Intent Activity Here
+                drawer.closeDrawer(GravityCompat.START)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_users -> {
+                //Add Users Intent Activity Here
+                drawer.closeDrawer(GravityCompat.START)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        drawer.closeDrawer(GravityCompat.START)
+        false
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
