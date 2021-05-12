8af2cc075dc8b2392c14ba1b9d2f0dccf79fe88f
dylandasilva1999 <55394640+dylandasilva1999@users.noreply.github.com>
2021-05-12 22:57:13 +0200

    Update User Class With More Fields

--- app/src/main/java/com/example/babble/model/User.kt
+++ app/src/main/java/com/example/babble/model/User.kt
@@ -3,5 +3,11 @@ package com.example.babble.model
 class User (
     val id: String = "",
     val fullName: String = "",
-    val email: String = ""
+    val email: String = "",
+    val profileImage: String = "",
+    val status: String = "",
+    val search: String = "",
+    val instagram: String = "",
+    val facebook: String = "",
+    val twitter: String = ""
 )
